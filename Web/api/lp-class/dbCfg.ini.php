a6387508b5c75d6ebe147e00e8e6d639257a10ef
whtsky <whtsky@vip.qq.com>
2011-11-27 17:17:34 +0800

    改过来。。

--- Web/api/lp-class/dbCfg.ini.php
+++ Web/api/lp-class/dbCfg.ini.php
@@ -1,10 +1,10 @@
 ;<?php /*
-;下面各项分别是 主机、数据库名、用户名、密码、字符集、整理类型
+;下面各项分别是 主机、数据表名、用户名、密码、字符集、整理类型
 [MySQL]
-host=127.0.0.1
-dbname=jymain
-user=root
-pwd=jybox.net
+host=主机地址
+dbname=数据表名
+user=用户名
+pwd=密码
 charset=utf8
 collate=utf8_general_ci
 ;*/ ?>
