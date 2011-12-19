296145c04817f200fb9fc9fa9222eea749ebbfb1
jybox <m@jybox.net>
2011-12-19 23:57:41 +0800

    改为空格缩紧（貌似这个文件里的加密算法仍有问题）

--- Web/api/login.php
+++ Web/api/login.php
@@ -7,33 +7,36 @@ $pkey="0-ms-0.1";
 
 if($_GET["mode"]=="post")
 {
-	switch($_POST["do"])
-	{
-		case "login":
-			$conn=new MySQL();
-			$rs=$conn->SQL(SafeSQL("SELECT * FROM `jypw_members` WHERE `username`='%1'")->string($_POST["uname"]));
-			$rs->read();
-			$time=$_POST["time"];
-			$time=$time-($time%10);
-            if($_GET["time"]=="yes"){
-    			$spwd=md5(md5($time).md5($_POST["uname"]).($rs->password));
-	    		$dpwd=md5($pkey.$spwd);
-            }else{
+    switch($_POST["do"])
+    {
+        case "login":
+            $conn=new MySQL();
+            $rs=$conn->SQL(SafeSQL("SELECT * FROM `jypw_members` WHERE `username`='%1'")->string($_POST["uname"]));
+            $rs->read();
+            $time=$_POST["time"];
+            $time=$time-($time%10);
+            if($_GET["time"]=="yes")
+            {
+                $spwd=md5(md5($time).md5($_POST["uname"]).($rs->password));
+                $dpwd=md5($pkey.$spwd);
+            }
+            else
+            {
                 $dpwd=md5(md5($_POST["uname"]).md5($rs->password));
             }
-			if($_POST["pwd"]==$dpwd)
-			{
-				echo "1\n";
-			}
-			else
-			{
-				echo "0\n";
-			}
-			echo $_POST["uname"]."\n";
-			echo $_POST["listname"]."\n";
-			echo $_POST["clientver"]."\n";
-			echo $_POST["clientname"]."\n";
-			break;
+            if($_POST["pwd"]==$dpwd)
+            {
+                echo "1\n";
+            }
+            else
+            {
+                echo "0\n";
+            }
+            echo $_POST["uname"]."\n";
+            echo $_POST["listname"]."\n";
+            echo $_POST["clientver"]."\n";
+            echo $_POST["clientname"]."\n";
+            break;
 	}
 }
 ?>
