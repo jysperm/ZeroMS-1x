ff517323ca5faf2912f0c3d2babb364936d3e1b4
whtsky <whtsky@vip.qq.com>
2011-12-23 21:30:41 +0800

    TAB转空格
    其实我是故意的

--- Web/api/login.php
+++ Web/api/login.php
@@ -7,32 +7,32 @@ $pkey="0-ms-0.1";
 
 if($_GET["mode"]=="post")
 {
-	switch($_POST["do"])
-	{
-		case "login":
-			$conn=new MySQL();
-			$rs=$conn->SQL(SafeSQL("SELECT * FROM `jypw_members` WHERE `username`='%1'")->string($_POST["uname"]));
-			$rs->read();
-			$time=$_POST["time"];
-			$time=$time-($time%10);
+    switch($_POST["do"])
+    {
+        case "login":
+            $conn=new MySQL();
+            $rs=$conn->SQL(SafeSQL("SELECT * FROM `jypw_members` WHERE `username`='%1'")->string($_POST["uname"]));
+            $rs->read();
+            $time=$_POST["time"];
+            $time=$time-($time%10);
             if($_GET["time"]=="yes")
-    			$spwd=md5(md5($time).md5($_POST["uname"]).($rs->password));
+                $spwd=md5(md5($time).md5($_POST["uname"]).($rs->password));
             else
                 $spwd=md5(md5($_POST["uname"]).md5($rs->password));
             $dpwd=md5($pkey.$dpwd);
-			if($_POST["pwd"]==$dpwd)
-			{
-				echo "1\n";
-			}
-			else
-			{
-				echo "0\n";
-			}
-			echo $_POST["uname"]."\n";
-			echo $_POST["listname"]."\n";
-			echo $_POST["clientver"]."\n";
-			echo $_POST["clientname"]."\n";
-			break;
-	}
+            if($_POST["pwd"]==$dpwd)
+            {
+                echo "1\n";
+            }
+            else
+            {
+                echo "0\n";
+            }
+            echo $_POST["uname"]."\n";
+            echo $_POST["listname"]."\n";
+            echo $_POST["clientver"]."\n";
+            echo $_POST["clientname"]."\n";
+            break;
+    }
 }
 ?>
