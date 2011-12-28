508ea16e9eaa81ffdf788cf26e72e3c04e1be925
whtsky <whtsky@vip.qq.com>
2011-12-28 21:31:53 +0800

    在Linux下，可以使用-nodaemon参数让服务器运行在非守护进程模式下（便于调试）

--- Qt4-Server/main.cpp
+++ Qt4-Server/main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <QCoreApplication>
 #include <QDir>
 #include <QTextStream>
@@ -22,27 +23,32 @@ QTextStream cout(stdout);
 int main(int argc, char *argv[])
 {       
     #if defined Q_OS_LINUX  
-    int pid; 
-    int i;
-    if(pid=fork()) 
-        exit(0);//是父进程，结束父进程 
-    else if(pid< 0) 
-        exit(1);//fork失败，退出 
-    
-    //是第一子进程，后台继续执行
-    setsid();//第一子进程成为新的会话组长和进程组长并和控制终端分离 
-    
-    if(pid=fork()) 
-        exit(0);//是第一子进程，结束第一子进程 
-    else if(pid< 0) 
-        exit(1);//fork失败，退出 
-    
-    //是第二子进程，继续 
-    //第二子进程不再是会话组长
-    for(i=0;i< NOFILE; i)//关闭打开的文档描述符 
-        close(i); 
-    chdir("/tmp");//改变工作目录到/tmp 
-    umask(0);//重设文档创建掩模 
+    if(argc > 1 && strcmp(argv[1], "-nodaemon") == 0)
+        cout << "Running Server without daemon \n";
+        cout.flush();
+    else{
+        int pid; 
+        int i;
+        if(pid=fork()) 
+            exit(0);//是父进程，结束父进程 
+        else if(pid< 0) 
+            exit(1);//fork失败，退出 
+        
+        //是第一子进程，后台继续执行
+        setsid();//第一子进程成为新的会话组长和进程组长并和控制终端分离 
+        
+        if(pid=fork()) 
+            exit(0);//是第一子进程，结束第一子进程 
+        else if(pid< 0) 
+            exit(1);//fork失败，退出 
+        
+        //是第二子进程，继续 
+        //第二子进程不再是会话组长
+        for(i=0;i< NOFILE; i)//关闭打开的文档描述符 
+            close(i); 
+        chdir("/tmp");//改变工作目录到/tmp 
+        umask(0);//重设文档创建掩模 
+    }
     #endif // Q_OS_LINUX  
         
     QCoreApplication a(argc, argv);
