49d52f0a8296a519717d6eb22b13627e664b5c34
jybox <m@jybox.net>
2012-01-20 21:39:42 +0800

    零毫秒2.x开工 建库SQL(仅一部分)

---
+++ Web/DataBase.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE IF NOT EXISTS `group` (
+  `gid` int(11) NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(255) NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  `master` varchar(255) NOT NULL,
+  `regtime` int(11) unsigned NOT NULL,
+  `website` text NOT NULL,
+  `info` text NOT NULL,
+  `avater` varchar(32) NOT NULL,
+  PRIMARY KEY (`gid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `group_member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(255) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `isadmin` tinyint(1) NOT NULL,
+  `isdeny` tinyint(1) NOT NULL,
+  `regtime` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(255) NOT NULL,
+  `pwd` text NOT NULL,
+  `lastlogintime` int(10) unsigned NOT NULL,
+  `lastloginip` text NOT NULL,
+  `regtime` int(11) unsigned NOT NULL,
+  `onlinetime` int(11) NOT NULL,
+  `website` text NOT NULL,
+  `info` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `avatar` varchar(32) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
